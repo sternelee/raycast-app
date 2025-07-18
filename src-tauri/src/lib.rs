745bb0d746706d9603340893cf248d47aec45ecb
sternelee <sternelee@gmail.com>
2025-07-18 11:55:40 +0800

    feat: remove global shortcuts for twice

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -251,7 +251,6 @@ pub fn run() {
             }
         }))
         .plugin(tauri_plugin_deep_link::init())
-        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_opener::init())
